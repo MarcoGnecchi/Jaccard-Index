e24f5790e023061bd9e38ad71ff58f20c824bd3e
Marco Gnecchi <magnecch@cisco.com>
2017-04-07 16:51:24 +0100

    first commit

---
+++ src/main/java/com/cisco/JaccardIndex.java
@@ -0,0 +1,13 @@
+package com.cisco;
+
+/**
+ * Hello world!
+ *
+ */
+public class JaccardIndex
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
